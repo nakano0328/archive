bd53532a214cbac194054025faf9af3f54c370dc
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-02 16:00:09 +0900

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,14 +3,9 @@ const nextConfig = {
     // ↓ next build を実行する際に Static Export を利用します。
     output: 'export',
     // サブディレクトリのパスを設定します。
-    basePath: '/nextjs',
+    //basePath: '/nextjs',
     // 静的アセットのパスを設定します。
-    assetPrefix: '/nextjs/',
+    //assetPrefix: '/nextjs/',
 };
 
-export default {
-  output: 'export',
-  distDir: 'out',  // 静的ファイルの出力先
-  basePath: '/nextjs',
-  assetPrefix: '/nextjs/',
-};
+export default nextConfig;
