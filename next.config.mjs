fff02ee331a27010ad8fd6e63e02b11f8867a540
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-03 16:15:15 +0900

    srcディレクトリを消しました

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    // ↓ next build を実行する際に Static Export を利用します。
-    output: 'export',
-    // サブディレクトリのパスを設定します。
-    //basePath: '/nextjs',
-    // 静的アセットのパスを設定します。
-    //assetPrefix: '/nextjs/',
-};
+const nextConfig = {};
 
 export default nextConfig;
