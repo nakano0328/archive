9e229dc76741bb5762102af667ada9046f249d55
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-04 14:14:29 +0900

    6.5

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,9 +1,10 @@
 export default function NotFound() {
-    return
-    <h1>
-        <p>
-            ここは存在しないページやで！<br>
+    return (
+        <h1>
+            <p>
+                ここは存在しないページやで！<br />
                 いわゆる404 Not Foundや！！
-        </p>
-    </h1>
+            </p>
+        </h1>
+    );
 }
