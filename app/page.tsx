a295d2de83622afd7004e5d7f9088443f79c9b4d
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-07 11:26:42 +0900

    リンク先の修正

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ const Home = () => {
     <div>
       <header>
         <h1>
-          <a href="@/">Mathlium</a>
+          <a href="@/app">Mathlium</a>
         </h1>
         <p>このサイトでは、数学の基礎から応用までをカバーします。</p>
       </header>
