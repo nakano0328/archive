126abb39021ea329cf0182a8b1b9eae927fb8637
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-08 21:12:22 +0900

    要らないimportの削除

--- app/components/Breadcrumb.tsx
+++ app/components/Breadcrumb.tsx
@@ -1,17 +1,3 @@
-'use client';
-
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { useEffect, useState } from 'react';
-
-// パス名と表示する名前を対応させるマッピング
-const breadcrumbMap: { [key: string]: string } = {
-  'linear_algebra': '線形代数',
-  'dotproduct': '内積',
-  // 必要に応じて他のパスと名前も追加できます
-  // 'another_path': 'Another Display Name',
-};
-
 interface BreadcrumbProps {
   serverData: {
     [key: string]: { title: string };
@@ -37,4 +23,3 @@ export default function Breadcrumb({ serverData }: BreadcrumbProps) {
     </nav>
   );
 }
-
