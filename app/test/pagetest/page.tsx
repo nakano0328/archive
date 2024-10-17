9d7ed71da263ed917558b71ed630007ed7e3d334
nakano0328 <n.mitsu.0328@icloud.com>
2024-10-17 14:23:07 +0900

    共有できそう

--- app/test/pagetest/page.tsx
+++ app/test/pagetest/page.tsx
@@ -1,3 +1,14 @@
-export default function Page() {
-    return <h1>Page Test</h1>
-}
+import Link from 'next/link';
+
+const HomePage: React.FC = () => {
+    return (
+        <div>
+            <h1>以下はipynbのテストです。</h1>
+            <Link href="https://colab.research.google.com/github/jeonglabo/nextjs/blob/main/notebook/linear_algebra/dotproduct/command1.ipynb">
+                Go to About Page
+            </Link>
+        </div>
+    );
+};
+
+export default HomePage;
