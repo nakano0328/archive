8604a21ba2cfaef7c55dc5b2491fa70adb6560d3
nakano0328 <n.mitsu.0328@icloud.com>
2024-12-29 23:43:33 +0900

    不要なインポートを削除し、ページコンテンツの構造を簡素化しました。

--- app/linear_algebra/contents/pseudoinverse.tsx
+++ app/linear_algebra/contents/pseudoinverse.tsx
@@ -1,17 +1,7 @@
-import { metadata } from "@/app/linear_algebra/metadata";
 import CenteredEquation from "@/app/components/CenteredEquation";
 import { InlineMath } from "react-katex";
-import ImageModal from "@/app/components/ImageModal";
-import CustomLink from "@/app/components/CustomLink";
 
 export default function pageContent() {
-  const pagename: string = "pseudoinverse"; //ここを変更
-
-  const metaData = metadata[pagename];
-  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
-  const imagePath = `${basePath}/${metaData.topic}/${pagename}`;
-  const notePath = `https://colab.research.google.com/github/jeonglabo/nextjs/blob/main/notebook/${metaData.topic}/${pagename}`;
-
   return (
     <>
       <h2 className="caption">類似行列 (Similar Matrix)</h2>
