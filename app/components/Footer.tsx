6ce5243d12319bded8401da36a85156e41cf4621
nakano0328 <n.mitsu.0328@icloud.com>
2025-01-07 18:19:44 +0900

    Update Footer.tsx
    Signed-off-by: nakano0328 <n.mitsu.0328@icloud.com>

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer
       className="footer"
     >
-      <p>&copy; 2025 {siteTitle}. 私が作成しています。</p>
+      <p>&copy; 2024-2025  {siteTitle}.</p>
     </footer>
   );
 };
